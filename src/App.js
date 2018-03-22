da5b0dc137b0f45cf458d7b11608bffc1840412d
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-22 15:16:42 +0700

    init weather

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Weather } from './components/Weather';
+
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return <Weather />
+  }
+}
---
+++ src/components/WeatherMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const WeatherMessage = () => (
+    <div className="form-group">
+        <h3>SaiGon is now 30<sup>o</sup>C</h3>
+    </div>
+);
