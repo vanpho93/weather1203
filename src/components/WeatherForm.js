49192151dbceea8117598f812eeb27be65dcd014
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-22 15:32:43 +0700

    getTemp

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=';
 
 export class WeatherForm extends Component {
     constructor(props) {
         super(props);
         this.state = { txtCityName: '' };
+        this.getTemp = this.getTemp.bind(this);
     }
+
+    getTemp() {
+        const { txtCityName } = this.state;
+        axios.get(URL + txtCityName)
+        .then(x => alert(x.data.main.temp));
+    }
+
     render() {
         return (
             <div className="form-group">
@@ -16,7 +27,7 @@ export class WeatherForm extends Component {
                         onChange={evt => this.setState({ txtCityName: evt.target.value })}
                     />
                 </div>
-                <button className="btn btn-success form-control">
+                <button className="btn btn-success form-control" onClick={this.getTemp}>
                     Get Weather
                 </button>
             </div>
